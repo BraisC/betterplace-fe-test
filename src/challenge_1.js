585d1d85972b8397fda1a28f150de286ffc98ad4
Brais <braiscao.dev@gmail.com>
2021-08-29 13:55:53 +0200

    Challenge 1

--- src/challenge_1.js
+++ src/challenge_1.js
@@ -1,5 +1,20 @@
 // Challenge 1 solution
 
 export default function tobiesFriends(n, scores) {
-  // place your solution here
+  //it doesnt say that i can't sort the array of scores so...
+  const scoresInOrder = [...scores].sort((a, b) => b - a);
+  let numberOfCalls = 0;
+  for(let i=0; i<= n ; i++) {
+    console.log(`Called ${scoresInOrder[i]}`)
+    scoresInOrder[i] = Math.min(...scoresInOrder)
+    for(let j=i+1; j <= n; j++) {
+      scoresInOrder[j]++;
+    }
+    numberOfCalls++;
+  }
+
+  return numberOfCalls; //this is the same as n as you only have to iterate over the sorted array to call of his friends, you could just return "n" if you only need this and don't actually need to modify the scores
 }
+
+//What are the function's time & space complexities?
+//The complexity in O notation is O(n^2)
